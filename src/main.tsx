205a11798e92703ae0c7992691ea35768042e8ba
bolajithe2nd <bellobolajithe2nd@gmail.com>
2023-12-27 22:45:57 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ WebApp.ready();
 // Event occurs whenever theme settings are changed in the user's Telegram app (including switching to night mode).
 WebApp.onEvent("themeChanged", function () {
   document.body.setAttribute("data-color-scheme", WebApp.colorScheme);
-  WebApp.setHeaderColor("bg_color");
+  WebApp.setHeaderColor("secondary_bg_color");
 });
 
 WebApp.setHeaderColor(WebApp.themeParams.secondary_bg_color);
